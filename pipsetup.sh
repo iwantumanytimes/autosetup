7c782bf044402deba77d22c9ae8e67c0ba0a586b
iwantumanytimes <15810623325@139.com>
2016-09-14 14:26:08 +0800

    自动设置supervisor的配置文件

--- pipsetup.sh
+++ pipsetup.sh
@@ -1,7 +1,14 @@
 echo '...you will  steup  python pip...'
 pipversiondefault=1.5.5
 yum  install   python-setuptools
+
 wget --no-check-certificate https://github.com/pypa/pip/archive/$pipversiondefault.tar.gz
+
 tar zvxf $pipversiondefault.tar.gz    #解压文件
 cd pip-$pipversiondefault/
 python setup.py install
+
+
+echo '...you will  steup  supervisor...'
+pip  install  supervisor
+echo_supervisord_conf >/etc/supervisor.conf 
