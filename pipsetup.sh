c4b9554d5e45c26c92fefabf1f62e067b8d414f6
iwantumanytimes <15810623325@139.com>
2016-09-14 11:35:53 +0800

    pip版本替换为变量控制

--- pipsetup.sh
+++ pipsetup.sh
@@ -1,6 +1,7 @@
 echo '...you will  steup  python pip...'
-yum install   python-setuptools
-wget --no-check-certificate https://github.com/pypa/pip/archive/1.5.5.tar.gz
-tar zvxf 1.5.5.tar.gz    #解压文件
-cd pip-1.5.5/
+pipversiondefault=1.5.5
+yum  install   python-setuptools
+wget --no-check-certificate https://github.com/pypa/pip/archive/$pipversiondefault.tar.gz
+tar zvxf $pipversiondefault.tar.gz    #解压文件
+cd pip-$pipversiondefault/
 python setup.py install
