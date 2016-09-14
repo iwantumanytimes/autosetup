ebacb0a72055bb8e83944c0c5850efa726037ca7
iwantumanytimes <15810623325@139.com>
2016-09-14 11:31:40 +0800

    更新提示

--- pipsetup.sh
+++ pipsetup.sh
@@ -1,4 +1,4 @@
-echo 'you will  steup  python pip'
+echo '...you will  steup  python pip...'
 yum install   python-setuptools
 wget --no-check-certificate https://github.com/pypa/pip/archive/1.5.5.tar.gz
 tar zvxf 1.5.5.tar.gz    #解压文件
